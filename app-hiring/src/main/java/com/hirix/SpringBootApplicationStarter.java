55bc5a648340144bc67d1a03fa0b11ac1001e068
victpol <vpolik10@gmail.com>
2023-05-03 09:09:53 +0200

    initial commit

---
+++ app-hiring/src/main/java/com/hirix/SpringBootApplicationStarter.java
@@ -0,0 +1,21 @@
+package com.hirix;
+
+import com.hirix.configuration.ApplicationConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication(scanBasePackages = "com.hirix")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableWebMvc
+@Import(
+        {ApplicationConfig.class}
+                //HibernateConfiguration.class}
+)
+public class SpringBootApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationStarter.class, args);
+    }
+}
