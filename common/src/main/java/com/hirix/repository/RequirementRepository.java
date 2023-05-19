92de5add133ba7a9b343de4604ca96390929779a
victpol <vpolik10@gmail.com>
2023-05-19 14:03:38 +0200

    update business method to find requirements by skill id with max_salary to requirement controller

--- common/src/main/java/com/hirix/repository/RequirementRepository.java
+++ common/src/main/java/com/hirix/repository/RequirementRepository.java
@@ -52,7 +52,20 @@ public interface RequirementRepository extends JpaRepository<Requirement, Long> 
             " r.profession.id = :professionId and" +
             " r.specialization.id = :specializationId and" +
             " r.rank.id = :rankId and" +
-            " r.position.id = :positionId)"
+            " r.position.id = :positionId) and" +
+                    " r.locationOffered.id in (select l.locationId from  LinkSkillsLocations l where l.skillId = :skillId) and" +
+                    " r.experience <= :experience and" +
+                    " r.active = :isActive and" +
+                    " r.recommendations <= :recommendations and" +
+                    " r.salary >= :salaryMin and" +
+                    " r.salary <= :salaryMax and" +
+                    " r.term >= :termMin and" +
+                    " r.term <= :termMax and" +
+                    " r.industry.id = :industryId and" +
+                    " r.profession.id = :professionId and" +
+                    " r.specialization.id = :specializationId and" +
+                    " r.rank.id = :rankId and" +
+                    " r.position.id = :positionId"
     )
     List<Requirement> findRequirementsBySkillIdAndSalaryMaxQuery(Integer experience, boolean isActive,
                                                 Integer recommendations, Integer salaryMin, Integer salaryMax, Integer termMin,
