c27d295d0a3d978584f7acc4cecb1107f2f88585
victpol <vpolik10@gmail.com>
2023-05-24 10:14:00 +0200

    add logger aspects to all repository methods

---
+++ common/src/main/java/com/hirix/aspect/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.hirix.aspect;
+
+@org.springframework.stereotype.Component
+@org.aspectj.lang.annotation.Aspect
+public class LoggingAspect {
+
+    private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(LoggingAspect.class);
+
+    @org.aspectj.lang.annotation.Before("aroundRepositoryPointcut()")
+    public void logBefore(org.aspectj.lang.JoinPoint joinPoint) {
+        log.info("Method " + joinPoint.getSignature().getName() + " start");
+    }
+
+    @org.aspectj.lang.annotation.AfterReturning(pointcut = "aroundRepositoryPointcut()")
+    public void doAccessCheck(org.aspectj.lang.JoinPoint joinPoint) {
+        log.info("Method " + joinPoint.getSignature().getName() + " finished");
+    }
+
+    @org.aspectj.lang.annotation.Pointcut("execution(* com.hirix.repository.*.*(..))")
+    public void aroundRepositoryPointcut() {
+    }
+}
